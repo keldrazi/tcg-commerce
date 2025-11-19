783d67ad5d430b7f2d0edabc362d4f4a05d43a53
Kevin Schmidt <kevin.schmidt@rrdigital.io>
2025-11-19 08:53:34 -0500

    UPDATE 11/19/2025

--- src/tcgdb/modules/tcgdb/api/mtg/price/current/tcgdb.mtg.price.current.module.ts
+++ src/tcgdb/modules/tcgdb/api/mtg/price/current/tcgdb.mtg.price.current.module.ts
@@ -3,16 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { TCGdbMTGPriceCurrentService } from './tcgdb.mtg.price.current.service';
 import { TCGdbMTGPriceCurrentController } from "./tcgdb.mtg.price.current.controller";
-import { TCGdbMTGPriceHistoryModule } from 'src/tcgdb/modules/tcgdb/api/mtg/price/history/tcgdb.mtg.price.history.module';
-import { TCGPlayerMTGPriceModule } from 'src/tcgdb/modules/tcgplayer/mtg/price/tcgplayer.mtg.price.module';
-import { TCGdbMTGCardModule } from 'src/tcgdb/modules/tcgdb/api/mtg/card/tcgdb.mtg.card.module';
 import { TCGdbAPIUtilModule } from 'src/tcgdb/modules/tcgdb/api/util/tcgdb.api.util.module';
 
 @Module({
     imports: [
-        TCGPlayerMTGPriceModule,
-        TCGdbMTGCardModule,
-        TCGdbMTGPriceHistoryModule,
         TCGdbAPIUtilModule,
         ConfigModule,
         HttpModule,
--- src/tcgdb/modules/tcgdb/api/mtg/price/current/tcgdb.mtg.price.current.service.ts
+++ src/tcgdb/modules/tcgdb/api/mtg/price/current/tcgdb.mtg.price.current.service.ts
@@ -1,8 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { TCGPlayerMTGPriceService } from 'src/tcgdb/modules/tcgplayer/mtg/price/tcgplayer.mtg.price.service';
-import { TCGdbMTGCardService } from 'src/tcgdb/modules/tcgdb/api/mtg/card/tcgdb.mtg.card.service';
-import { TCGdbMTGPriceHistoryService } from 'src/tcgdb/modules/tcgdb/api/mtg/price/history/tcgdb.mtg.price.history.service';
-import { TCGdbMTGPricesCurrentDTO, TCGdbMTGPriceCurrentDTO } from './dto/tcgdb.mtg.price.current.dto';
 import { TCGdbAPIUtilService } from 'src/tcgdb/modules/tcgdb/api/util/tcgdb.api.util.service';
 import { ConfigService } from '@nestjs/config';
 import { HttpService } from '@nestjs/axios';
@@ -13,9 +9,6 @@ import { ForbiddenException } from '@nestjs/common';
 export class TCGdbMTGPriceCurrentService {
 
     constructor(
-        private tcgPlayerMTGPriceService: TCGPlayerMTGPriceService,
-        private tcgdbMTGCardService: TCGdbMTGCardService,
-        private tcgdbMTGPriceHistoryService: TCGdbMTGPriceHistoryService,
         private tcgdbAPIUtilService: TCGdbAPIUtilService,
         private configService: ConfigService,
         private httpService: HttpService,
@@ -23,51 +16,6 @@ export class TCGdbMTGPriceCurrentService {
 
 
     private tcgdbAPIURL = this.configService.get('TCGDB_API_URL');
-    /*
-    async getTCGdbMTGPricesCurrentByCardId(cardId: string) {
-        
-        const tcgdbMTGPriceCurrents = await this.tcgdbMTGPriceCurrentRepository.find({
-            where: {
-                tcgdbMTGCardId: cardId,
-            }
-        });
-
-        let tcgdbMTGPriceCurrentDTOs: TCGdbMTGPriceCurrentDTO[] = [];
-
-        for(let i = 0; i < tcgdbMTGPriceCurrents.length; i++) {
-            let tcgdbMTGPriceCurrent = tcgdbMTGPriceCurrents[i];
-
-            let tcgdbMTGPriceCurrentDTO: TCGdbMTGPriceCurrentDTO = { ...tcgdbMTGPriceCurrent };
-            
-            tcgdbMTGPriceCurrentDTOs.push(tcgdbMTGPriceCurrentDTO);
-
-        }
-
-        let tcgdbMTGPriceCurrentsDTO: TCGdbMTGPricesCurrentDTO = {
-            tcgdbMTGPricesCurrent: tcgdbMTGPriceCurrentDTOs,
-        }
-
-        return tcgdbMTGPriceCurrentsDTO;
-
-    } 
-    async getTCGdbMTGPricesCurrentByCardIdAndProductCardPrinting(tcgdbMTGCardId: string, tcgdbMTGPriceCurrentSubTypeName: string) {
-        const tcgdbMTGPriceCurrent = await this.tcgdbMTGPriceCurrentRepository.findOne({
-            where: {
-                tcgdbMTGCardId: tcgdbMTGCardId,
-                tcgdbMTGPriceCurrentSubTypeName: tcgdbMTGPriceCurrentSubTypeName,
-            }
-        });
-
-        if(tcgdbMTGPriceCurrent == null) {
-            return null;
-        }
-
-        let tcgdbMTGPriceCurrentDTO: TCGdbMTGPriceCurrentDTO = { ...tcgdbMTGPriceCurrent };
-
-        return tcgdbMTGPriceCurrentDTO;
-
-    }
-    */
     
     async getTCGdbMTGPricesCurrentBySetCode(setCode: string) {
         
@@ -87,7 +35,8 @@ export class TCGdbMTGPriceCurrentService {
         return data;
 
     }
-        async getTCGdbMTGPricesCurrentBySetId(tcgdbSetId: string) {
+    
+    async getTCGdbMTGPricesCurrentBySetId(tcgdbSetId: string) {
         
         //GET ALL TCGDB PRICES BY SET ID;
         const accessToken = await this.tcgdbAPIUtilService.getTCGdbAPIAccessToken();
@@ -104,59 +53,39 @@ export class TCGdbMTGPriceCurrentService {
 
         return data;
  
-    } 
-    /*
-    async createTCGdbMTGPricesCurrent() {
-
-        //REMOVE ALL CURRENT PRICES;
-        await this.tcgdbMTGPriceCurrentRepository.createQueryBuilder()
-            .delete()
-            .from(TCGdbMTGPriceCurrent)
-            .execute();
-
-
-        let tcgdbMTGPriceCurrentRecordCount = 0;
-        let tcgPlayerMTGPrices = await this.tcgPlayerMTGPriceService.getTCGPlayerMTGPricesToProcess();
-
-        //TO DO: CREATE AN ERROR TO RETURN;
-        if(tcgPlayerMTGPrices == null) {
-            return null;
-        }
-
-        for(let i = 0; i < tcgPlayerMTGPrices.length; i++) {
-            let tcgPlayerMTGPrice = tcgPlayerMTGPrices[i];
-            let tcgdbMTGCard = await this.tcgdbMTGCardService.getTCGdbMTGCardByTCGPlayerId(tcgPlayerMTGPrice.tcgPlayerMTGPriceProductId);
-
-            //TO DO: CREATE AN ERROR TO LOG;
-            if(tcgdbMTGCard == null) {
-                continue;
-            }
-
-            const newTCGdbMTGPrice = this.tcgdbMTGPriceCurrentRepository.create({
-                tcgdbMTGCardId: tcgdbMTGCard.tcgdbMTGCardId,
-                tcgdbMTGPriceCurrentTCGPlayerId: tcgPlayerMTGPrice.tcgPlayerMTGPriceProductId,
-                tcgdbMTGPriceCurrentSetCode: tcgdbMTGCard.tcgdbMTGCardSetCode,
-                tcgdbMTGPriceCurrentLowPrice: tcgPlayerMTGPrice.tcgPlayerMTGPriceLowPrice,
-                tcgdbMTGPriceCurrentMidPrice: tcgPlayerMTGPrice.tcgPlayerMTGPriceMidPrice,
-                tcgdbMTGPriceCurrentHighPrice: tcgPlayerMTGPrice.tcgPlayerMTGPriceHighPrice,
-                tcgdbMTGPriceCurrentMarketPrice: tcgPlayerMTGPrice.tcgPlayerMTGPriceMarketPrice,
-                tcgdbMTGPriceCurrentDirectLowPrice: tcgPlayerMTGPrice.tcgPlayerMTGPriceDirectLowPrice,
-                tcgdbMTGPriceCurrentSubTypeName: tcgPlayerMTGPrice.tcgPlayerMTGPriceSubTypeName,
-            });
-
-            await this.tcgdbMTGPriceCurrentRepository.save(newTCGdbMTGPrice);
-            await this.tcgdbMTGPriceHistoryService.createTCGdbMTGPricesHistory(tcgdbMTGCard, tcgPlayerMTGPrice);
-            
-            tcgdbMTGPriceCurrentRecordCount++;
-        }
-
-        await this.tcgPlayerMTGPriceService.updateTCGPlayerMTGPricesIsProcessed();
-        await this.tcgPlayerMTGPriceService.deleteTCGPlayerMTGPricesIsProcessed();
+    }
+    
+    async getTCGdbMTGPricesCurrentByTCGdbId(tcgdbCardId: string) {
         
-        return tcgdbMTGPriceCurrentRecordCount;
+        const accessToken = await this.tcgdbAPIUtilService.getTCGdbAPIAccessToken();
+        const url = this.tcgdbAPIURL + '/tcgdb/mtg/price/current/tcgdb/id/' + tcgdbCardId;
+        const headers = { 'Authorization': 'Bearer ' + accessToken };
+        const response = this.httpService.get(url, { headers }).pipe(
+            map(response => response.data),
+            catchError(error => {
+                throw new ForbiddenException(error.response.data);
+            })
+        );
 
-    } 
-        */   
+        let data = await lastValueFrom(response);
+
+        return data;
+    }
+
+    async getTCGdbMTGPricesCurrentByTCGdbIdAndProductCardPrinting(tcgdbCardId: string, printingName: string) {
+        
+        const accessToken = await this.tcgdbAPIUtilService.getTCGdbAPIAccessToken();
+        const url = this.tcgdbAPIURL + '/tcgdb/mtg/price/current/tcgdb/id/' + tcgdbCardId + '/printing/' + printingName;
+        const headers = { 'Authorization': 'Bearer ' + accessToken };
+        const response = this.httpService.get(url, { headers }).pipe(
+            map(response => response.data),
+            catchError(error => {
+                throw new ForbiddenException(error.response.data);
+            })
+        );
+
+        let data = await lastValueFrom(response);
+
+        return data;
+    }
 }
-
-
