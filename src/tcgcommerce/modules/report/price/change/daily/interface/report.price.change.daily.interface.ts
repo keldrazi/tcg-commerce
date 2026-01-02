e565918c54d355356c7a89821cbc7f5cdb630276
Kevin Schmidt <kevin.schmidt@rrdigital.io>
2026-01-02 16:39:00 -0500

    UPDATE 1/2/2026

--- src/tcgcommerce/modules/report/price/change/daily/interface/report.price.change.daily.interface.ts
+++ src/tcgcommerce/modules/report/price/change/daily/interface/report.price.change.daily.interface.ts
@@ -1,10 +1,12 @@
 export interface ReportPriceChangeDailySettings {
+    reportPriceChangeDailyDefaultPOSVendorId: string;
+    reportPriceChangeDailyDefaultPOSVendorPriceOption: string;
     reportPriceChangeDailyDefaultMinimumPrice: number;
     reportPriceChangeDailyDefaultMaximumPrice: number;
     reportPriceChangeDailyDefaultMinimumPriceChange: number;
     reportPriceChangeDailyDefaultMinimumPercentChange: number;
     reportPriceChangeDailyDefaultMaximumRecordCount: number;
     reportPriceChangeDailyDefaultProductCardPrinting: string;
-    reportPriceChangeDailyDefaultPriceOption: string;
+    
 }
     
--- src/tcgcommerce/modules/report/price/change/daily/report.price.change.daily.service.ts
+++ src/tcgcommerce/modules/report/price/change/daily/report.price.change.daily.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportPriceChangeDailyDTO, UpdateReportPriceChangeDailyDTO, ReportPriceChangeDailyDTO } from './dto/report.price.change.daily.dto';
+import { ReportPriceChangeDailySettings } from './interface/report.price.change.daily.interface';
 import { ReportPriceChangeDaily } from 'src/typeorm/entities/tcgcommerce/modules/report/price/change/daily/report.price.change.daily.entity';
 import { ErrorMessageService } from 'src/system/modules/error/message/error.message.service';
 
@@ -24,8 +25,17 @@ export class ReportPriceChangeDailyService {
             return this.errorMessageService.createErrorMessage('REPORT_PRICE_CHANGE_DAILY_NOT_FOUND', 'Report price change daily was not found');
         }
 
-        let reportPriceChangeDailyDTO: ReportPriceChangeDailyDTO = ({ ...reportPriceChangeDaily });
-
+        let reportPriceChangeDailyDTO: ReportPriceChangeDailyDTO = new ReportPriceChangeDailyDTO();
+        reportPriceChangeDailyDTO.productLineId = reportPriceChangeDaily.productLineId;
+        reportPriceChangeDailyDTO.productTypeId = reportPriceChangeDaily.productTypeId;
+        reportPriceChangeDailyDTO.productVendorId = reportPriceChangeDaily.productVendorId;
+        reportPriceChangeDailyDTO.reportPriceChangeDailyId = reportPriceChangeDaily.reportPriceChangeDailyId;
+        reportPriceChangeDailyDTO.reportPriceTypeId = reportPriceChangeDaily.reportPriceTypeId;
+        reportPriceChangeDailyDTO.reportPriceChangeDailyName = reportPriceChangeDaily.reportPriceChangeDailyName;
+        reportPriceChangeDailyDTO.reportPriceChangeDailyDescription = reportPriceChangeDaily.reportPriceChangeDailyDescription;
+        reportPriceChangeDailyDTO.reportPriceChangeDailyCategories = JSON.parse(reportPriceChangeDaily.reportPriceChangeDailyCategories);
+        reportPriceChangeDailyDTO.reportPriceChangeDailySettings = JSON.parse(reportPriceChangeDaily.reportPriceChangeDailySettings) as ReportPriceChangeDailySettings;
+        
         return reportPriceChangeDailyDTO;
     }
     
@@ -45,10 +55,12 @@ export class ReportPriceChangeDailyService {
             return this.errorMessageService.createErrorMessage('REPORT_PRICE_CHANGE_DAILY_ALREADY_EXISTS', 'Report price change daily already exists');
         }
         
-        let newReportPriceChangeDaily = this.reportPriceChangeDailyRepository.create({ ...createReportPriceChangeDailyDTO });
-        newReportPriceChangeDaily = await this.reportPriceChangeDailyRepository.save(newReportPriceChangeDaily);
+        reportPriceChangeDaily = this.reportPriceChangeDailyRepository.create({ ...createReportPriceChangeDailyDTO });
+        reportPriceChangeDaily.reportPriceChangeDailyCategories = JSON.stringify(reportPriceChangeDaily.reportPriceChangeDailyCategories);
+        reportPriceChangeDaily.reportPriceChangeDailySettings = JSON.stringify(reportPriceChangeDaily.reportPriceChangeDailySettings);
+        reportPriceChangeDaily = await this.reportPriceChangeDailyRepository.save(reportPriceChangeDaily);
 
-        let reportPriceChangeDailyDTO = this.getReportPriceChangeDaily(newReportPriceChangeDaily.reportPriceChangeDailyId);
+        let reportPriceChangeDailyDTO = await this.getReportPriceChangeDaily(reportPriceChangeDaily.reportPriceChangeDailyId);
         
         return reportPriceChangeDailyDTO;
         
