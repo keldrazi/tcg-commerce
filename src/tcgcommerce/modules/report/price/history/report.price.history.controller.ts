3ab3df593a10c720769662e8c3c6aa38eb72d3af
Kevin Schmidt <kevin.schmidt@rrdigital.io>
2026-01-03 18:04:49 -0500

    UPDATE 1/3/2026

---
+++ src/tcgcommerce/modules/report/price/history/report.price.history.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Body, Put, Param, ParseIntPipe, Delete, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { CreateReportPriceHistoryDTO, UpdateReportPriceHistoryDTO } from './dto/report.price.history.dto';
+import { ReportPriceHistoryService } from './report.price.history.service';
+
+@Controller('report/price/history')
+export class ReportPriceHistoryController {
+
+    constructor(
+        private reportPriceHistoryService: ReportPriceHistoryService,
+    ) { }
+    
+
+    @Post('/create')
+    @UsePipes(new ValidationPipe())
+    async createReportPriceHistory(@Body() createReportPriceHistoryDTO: CreateReportPriceHistoryDTO) {
+        return await this.reportPriceHistoryService.createReportPriceHistory(createReportPriceHistoryDTO);
+    }
+
+    @Put('/update')
+    @UsePipes(new ValidationPipe())
+    async updateReportPriceHistory(@Body() updateReportPriceHistoryDTO: UpdateReportPriceHistoryDTO) {
+        return await this.reportPriceHistoryService.updateReportPriceHistory(updateReportPriceHistoryDTO);
+    }
+
+}
---
+++ src/tcgcommerce/modules/report/price/history/report.price.history.service.ts
@@ -0,0 +1,96 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportPriceHistoryDTO, UpdateReportPriceHistoryDTO, ReportPriceHistoryDTO } from './dto/report.price.history.dto';
+import { ReportPriceHistoryDefaultSettings, ReportPriceHistoryCategory } from './interface/report.price.history.interface';
+import { ReportPriceHistory } from 'src/typeorm/entities/tcgcommerce/modules/report/price/history/report.price.history.entity';
+import { ErrorMessageService } from 'src/system/modules/error/message/error.message.service';
+
+
+@Injectable()
+export class ReportPriceHistoryService {
+
+    constructor(
+        @InjectRepository(ReportPriceHistory) private reportPriceHistoryRepository: Repository<ReportPriceHistory>,
+        private errorMessageService: ErrorMessageService,
+    ) { }
+
+    async getReportPriceHistory(reportPriceHistoryId: string) {
+        let reportPriceHistory = await this.reportPriceHistoryRepository.findOne({ 
+            where: { 
+                reportPriceHistoryId: reportPriceHistoryId
+            } 
+        });
+
+        if (reportPriceHistory == null) {
+            return this.errorMessageService.createErrorMessage('REPORT_PRICE_HISTORY_NOT_FOUND', 'Report price history was not found');
+        }
+
+        let reportPriceHistoryDTO: ReportPriceHistoryDTO = new ReportPriceHistoryDTO();
+        reportPriceHistoryDTO.productLineId = reportPriceHistory.productLineId;
+        reportPriceHistoryDTO.productTypeId = reportPriceHistory.productTypeId;
+        reportPriceHistoryDTO.productVendorId = reportPriceHistory.productVendorId;
+        reportPriceHistoryDTO.reportPriceHistoryId = reportPriceHistory.reportPriceHistoryId;
+        reportPriceHistoryDTO.reportTypeId = reportPriceHistory.reportTypeId;
+        reportPriceHistoryDTO.reportPriceHistoryName = reportPriceHistory.reportPriceHistoryName;
+        reportPriceHistoryDTO.reportPriceHistoryDescription = reportPriceHistory.reportPriceHistoryDescription;
+        reportPriceHistoryDTO.reportPriceHistoryCategories = JSON.parse(reportPriceHistory.reportPriceHistoryCategories) as ReportPriceHistoryCategory[];
+        reportPriceHistoryDTO.reportPriceHistoryDefaultSettings = JSON.parse(reportPriceHistory.reportPriceHistoryDefaultSettings) as ReportPriceHistoryDefaultSettings;
+        
+        return reportPriceHistoryDTO;
+    }
+    
+    async createReportPriceHistory(createReportPriceHistoryDTO: CreateReportPriceHistoryDTO) {
+    
+        //CHECK TO SEE IF THE PRODUCT CARD TYPE ALREADY EXISTS;
+        let reportPriceHistory = await this.reportPriceHistoryRepository.findOne({ 
+            where: { 
+                productVendorId: createReportPriceHistoryDTO.productVendorId,
+                productLineId: createReportPriceHistoryDTO.productLineId,
+                productTypeId: createReportPriceHistoryDTO.productTypeId, 
+                reportPriceHistoryName: createReportPriceHistoryDTO.reportPriceHistoryName, 
+            } 
+        });
+        
+        if (reportPriceHistory != null) {
+            return this.errorMessageService.createErrorMessage('REPORT_PRICE_HISTORY_ALREADY_EXISTS', 'Report price history already exists');
+        }
+        
+        reportPriceHistory = this.reportPriceHistoryRepository.create({ ...createReportPriceHistoryDTO });
+        reportPriceHistory.reportPriceHistoryCategories = JSON.stringify(reportPriceHistory.reportPriceHistoryCategories);
+        reportPriceHistory.reportPriceHistoryDefaultSettings = JSON.stringify(reportPriceHistory.reportPriceHistoryDefaultSettings);
+        reportPriceHistory = await this.reportPriceHistoryRepository.save(reportPriceHistory);
+
+        let reportPriceHistoryDTO = await this.getReportPriceHistory(reportPriceHistory.reportPriceHistoryId);
+        
+        return reportPriceHistoryDTO;
+        
+    }
+
+    async updateReportPriceHistory(updateReportPriceHistoryDTO: UpdateReportPriceHistoryDTO) {
+                    
+        let reportPriceHistory = await this.reportPriceHistoryRepository.findOne({ 
+            where: { 
+                reportPriceHistoryId: updateReportPriceHistoryDTO.reportPriceHistoryId
+            } 
+        });
+
+        if (!reportPriceHistory) {
+            return this.errorMessageService.createErrorMessage('REPORT_PRICE_HISTORY_NOT_FOUND', 'Report price history was not found'); 
+        }
+        reportPriceHistory.reportTypeId = updateReportPriceHistoryDTO.reportTypeId;
+        reportPriceHistory.reportPriceHistoryName = updateReportPriceHistoryDTO.reportPriceHistoryName;
+        reportPriceHistory.reportPriceHistoryDescription = updateReportPriceHistoryDTO.reportPriceHistoryDescription;
+        reportPriceHistory.reportPriceHistoryCategories = JSON.stringify(updateReportPriceHistoryDTO.reportPriceHistoryCategories);
+        reportPriceHistory.reportPriceHistoryDefaultSettings = JSON.stringify(updateReportPriceHistoryDTO.reportPriceHistoryDefaultSettings);
+
+        reportPriceHistory.reportPriceHistoryUpdateDate = new Date();
+        
+        await this.reportPriceHistoryRepository.save(reportPriceHistory);
+        let reportPriceHistoryDTO = await this.getReportPriceHistory(reportPriceHistory.reportPriceHistoryId);
+
+        return reportPriceHistoryDTO;
+    
+    }
+    
+}
