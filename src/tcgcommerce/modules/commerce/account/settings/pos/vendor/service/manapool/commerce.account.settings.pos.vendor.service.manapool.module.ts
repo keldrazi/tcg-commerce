4fcd000b56f3c780f2ca0328b8652a040d20387d
Kevin Schmidt <kevin.schmidt@rrdigital.io>
2026-01-12 09:53:04 -0500

    UPDATE 1/12/2026

--- src/tcgcommerce/modules/commerce/account/settings/pos/vendor/service/manapool/commerce.account.settings.pos.vendor.service.manapool.module.ts
+++ src/tcgcommerce/modules/commerce/account/settings/pos/vendor/service/manapool/commerce.account.settings.pos.vendor.service.manapool.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { CommerceAccountSettingsPOSVendorServiceManaPoolService } from './commerce.account.settings.pos.vendor.service.manapool.service';
 import { CommerceAccountSettingsPOSVendorServiceManaPoolController } from './commerce.account.settings.pos.vendor.service.manapool.controller';
 import { CommerceAccountSettingsPOSVendorServiceManaPool } from 'src/typeorm/entities/tcgcommerce/modules/commerce/account/settings/pos/vendor/service/manapool/commerce.account.settings.pos.vendor.service.manapool.entity';
+import { POSVendorServiceManaPoolRestV1Module } from 'src/tcgcommerce/modules/pos/vendor/service/manapool/rest/v1/pos.vendor.service.manapool.rest.v1.module';
 import { ErrorMessageModule } from 'src/system/modules/error/message/error.message.module';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([CommerceAccountSettingsPOSVendorServiceManaPool]),
+        POSVendorServiceManaPoolRestV1Module,
         ErrorMessageModule
     ],
     controllers: [CommerceAccountSettingsPOSVendorServiceManaPoolController],
